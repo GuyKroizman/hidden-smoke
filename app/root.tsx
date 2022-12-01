b2ac0494b5bce6660cb481dc34ae8c47e738b5d8
GuyKroizman <guykr@wix.com>
2022-12-01 18:11:27 +0200

    tailwind things and snake bitter link

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,5 @@
+import styles from "./styles/app.css"
+
 import type { MetaFunction } from "@remix-run/node";
 import {
   Links,
@@ -8,9 +10,15 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
+export function links() {
+  return [
+    {rel: "stylesheet", href: styles},
+  ];
+}
+
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "Guy Kroizman",
   viewport: "width=device-width,initial-scale=1",
 });
 
