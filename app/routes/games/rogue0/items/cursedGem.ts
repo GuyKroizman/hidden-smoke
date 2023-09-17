2fe69bec4ca9761a13f89eaf7f43e2c97932594d
GuyKroizman <guykr@wix.com>
2023-09-17 21:26:21 +0300

    add a cursed gem

--- app/routes/games/rogue0/items/cursedGem.ts
+++ app/routes/games/rogue0/items/cursedGem.ts
@@ -1,21 +1,27 @@
 import type { Entity, EntityType } from "~/routes/games/rogue0/entity";
 import type { GameContext } from "~/routes/games/rogue0/context.client";
+import dungeon from "~/routes/games/rogue0/dungeon.client";
 
-export default class Gem implements Entity {
+export default class CursedGem implements Entity {
   x?: number;
   y?: number;
-  name: string = "Gem";
-  description: string = "A gem";
+  name: string = "Cursed Gem";
+  description: string = "A cursed gem that is now stuck to your hand. You can only remove it by finding a potion.";
   healthPoints: number = 0;
   tweens: number = 0;
   moving: boolean = false;
   sprite: Phaser.GameObjects.Sprite | undefined;
   type: EntityType = "item";
   tile = 4 * 49 + 23;
+  actionPoints: number = 1;
+  cursed: boolean = true;
+  context: GameContext;
+  active: boolean = false;
 
   constructor(context: GameContext, x?: number, y?: number) {
     this.x = x;
     this.y = y;
+    this.context = context;
 
     if (this.x && this.y) {
       let x = context.map!.tileToWorldX(this.x);
@@ -28,17 +34,26 @@ export default class Gem implements Entity {
   }
 
   damage() {
-    return 0;
+    return Phaser.Math.Between(4, 8);
   }
 
   turn() {
+    if (this.context.player?.items.includes(this)) {
+      this.active = true;
+      dungeon.log(this.context, `Cursed gem gives 1 damage to player. Find potion to cure.`);
+      this.context.player.healthPoints -= 1;
+      this.actionPoints = 0;
+    }
+
+    this.actionPoints = 0;
   }
 
   refresh() {
+    this.actionPoints = 1;
   }
 
   over() {
-    return true;
+    return this.actionPoints === 0;
   }
 
   attack(): number {
@@ -47,5 +62,4 @@ export default class Gem implements Entity {
 
   onDestroy() {
   }
-
 }
--- app/routes/games/rogue0/scene0.client.ts
+++ app/routes/games/rogue0/scene0.client.ts
@@ -6,6 +6,7 @@ import turnManager from "~/routes/games/rogue0/turnManager.client";
 import Skeleton from "~/routes/games/rogue0/monsters/skelaton.client";
 import LongSword from "~/routes/games/rogue0/items/longSword";
 import Gem from "~/routes/games/rogue0/items/gem";
+import CursedGem from "~/routes/games/rogue0/items/cursedGem";
 
 export class Scene0 extends Phaser.Scene {
   context: GameContext;
@@ -24,37 +25,38 @@ export class Scene0 extends Phaser.Scene {
     this.load.spritesheet("tiles", "/colored.png", {
       frameWidth: 16,
       frameHeight: 16,
-      spacing: 1,
+      spacing: 1
     });
   }
 
   create() {
-    console.log('create scene')
+    console.log("create scene");
     this.context.scene = this;
 
     dungeon.initialize(this.context);
 
     let player = new PlayerCharacter(this.context, 15, 15);
-    this.context.entities.push(player)
+    this.context.entities.push(player);
     this.context.player = player;
 
-    this.context.entities.push(new Skeleton(this.context,76, 10));
-    this.context.entities.push(new Skeleton(this.context,20, 20));
-    this.context.entities.push(new Skeleton(this.context,20, 10));
-    this.context.entities.push(new Skeleton(this.context,29, 24));
-    this.context.entities.push(new Skeleton(this.context,29, 20));
-    this.context.entities.push(new Gem(this.context,21, 21))
+    this.context.entities.push(new Skeleton(this.context, 76, 10));
+    this.context.entities.push(new Skeleton(this.context, 20, 20));
+    this.context.entities.push(new Skeleton(this.context, 20, 10));
+    this.context.entities.push(new Skeleton(this.context, 29, 24));
+    this.context.entities.push(new Skeleton(this.context, 29, 20));
+    this.context.entities.push(new Gem(this.context, 21, 21));
+    this.context.entities.push(new CursedGem(this.context, 15, 20));
 
-    this.context.entities.push(new LongSword(this.context, 18, 22))
+    this.context.entities.push(new LongSword(this.context, 18, 22));
 
-    this.events.emit('entities-created');
+    this.events.emit("entities-created");
 
     // Set camera, causes game viewport to shrink on the right side freeing
     // space for the UI scene.
-    let camera = this.cameras.main
-    camera.setViewport(0, 0, camera.worldView.width-200, camera.worldView.height)
-    camera.setBounds(0, 0, camera.worldView.width, camera.worldView.height)
-    camera.startFollow(this.context.player.sprite)
+    let camera = this.cameras.main;
+    camera.setViewport(0, 0, camera.worldView.width - 200, camera.worldView.height);
+    camera.setBounds(0, 0, camera.worldView.width, camera.worldView.height);
+    camera.startFollow(this.context.player.sprite);
   }
 
   update() {
