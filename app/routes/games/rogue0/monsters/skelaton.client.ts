d8ca6e707feaaa8a12fba4cafa000e2c6d18cfb9
GuyKroizman <guykr@wix.com>
2023-09-30 14:31:15 +0300

    fix attack delay

--- app/routes/games/rogue0/monsters/skelaton.client.ts
+++ app/routes/games/rogue0/monsters/skelaton.client.ts
@@ -2,7 +2,7 @@ import dungeon from "~/routes/games/rogue0/dungeon.client";
 import type { GameContext } from "~/routes/games/rogue0/context.client";
 import PF from "pathfinding";
 import level from "../level.client.js";
-import { Entity} from "~/routes/games/rogue0/entity";
+import { Entity } from "~/routes/games/rogue0/entity";
 import type { EntityType } from "~/routes/games/rogue0/entity";
 import Gem from "~/routes/games/rogue0/items/gem";
 import HolyPotion from "~/routes/games/rogue0/items/holyPotion";
@@ -12,12 +12,12 @@ export default class Skeleton extends Entity {
   private movementPoints: number;
   tile: number;
   moving: boolean;
-  readonly name: string;
+  readonly name: string = "Skeleton";
   description: string = "A skeleton";
-  type: EntityType;
+  type: EntityType = "enemy";
   actionPoints: number;
   healthPoints: number;
-  tweens: number = 50;
+  tweens: number = 2;
   UISprite?: Phaser.GameObjects.Sprite;
   UIText?: Phaser.GameObjects.Text;
 
@@ -38,9 +38,6 @@ export default class Skeleton extends Entity {
     this.moving = false;
 
     this.init(context, x, y);
-
-    this.name = "Skeleton";
-    this.type = "enemy";
   }
 
   refresh() {
@@ -63,16 +60,14 @@ export default class Skeleton extends Entity {
       const finder = new PF.AStarFinder();
       const path = finder.findPath(oldX!, oldY!, pX, pY, grid);
 
-      if (this.movementPoints > 0) {
-        if (path.length > 2) {
-          dungeon.moveEntityTo(this.context, this, path[1][0], path[1][1]);
-        }
-
-        this.movementPoints -= 1;
+      if (path.length > 2) {
+        dungeon.moveEntityTo(this.context, this, path[1][0], path[1][1]);
       }
 
+      this.movementPoints -= 1;
+
       if (this.actionPoints > 0) {
-        if (dungeon.distanceBetweenEntities(this, this.context.player) <= 2) {
+        if(path.length <= 2) {
           dungeon.attackEntity(this.context, this, this.context.player);
         }
         this.actionPoints -= 1;
@@ -113,8 +108,8 @@ export default class Skeleton extends Entity {
     }
 
     // loot
-    let x = this.x
-    let y = this.y
+    let x = this.x;
+    let y = this.y;
 
     let possibleLoot = [
       false,
@@ -123,18 +118,18 @@ export default class Skeleton extends Entity {
       false,
       Gem,
       HolyPotion,
-      BerserkPotion,
-    ]
+      BerserkPotion
+    ];
 
-    let lootIndex = Phaser.Math.Between(0,possibleLoot.length-1)
+    let lootIndex = Phaser.Math.Between(0, possibleLoot.length - 1);
     if (possibleLoot[lootIndex]) {
-      let item = possibleLoot[lootIndex]
+      let item = possibleLoot[lootIndex];
       // ignore the warning that false is not constructable because code is only
       // reachable if item is not false
       // @ts-ignore
-      this.context.entities.push(new item(this.context, x, y))
+      this.context.entities.push(new item(this.context, x, y));
       // @ts-ignore
-      dungeon.log(this.context, `${this.name} drops ${item.name}.`)
+      dungeon.log(this.context, `${this.name} drops ${item.name}.`);
     }
   }
 
